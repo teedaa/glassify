bf239480d2efdfe9144cddb4e8c76453fd883903
[carlos martinez] <theycallhellp@gmail.com>
2022-12-10 01:22:27 -0800

    attach authentication middleware to check logged in user

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,6 +3,26 @@ const jwt = require('jsonwebtoken');
 const expiration = '1h';
 
 module.exports = {
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+          token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+          return req;
+        }
+    
+        try {
+          const { data } = jwt.verify(token, process.env.SECRET, { maxAge: expiration });
+          req.user = data;
+        } catch {
+          console.log('Invalid token');
+        }
+    
+        return req;
+    },
     signToken: function ({ username, _id }) {
         const payload = { username, _id };
         return jwt.sign({ data: payload }, process.env.SECRET, { expiresIn: expiration });

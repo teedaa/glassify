cb894a502a08984eb290900f8e19ca1265e178df
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-12-10 19:08:41 -0500

    added mantine drawer component code

---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Drawer, Button, Group } from '@mantine/core';
+
+function sidebar() {
+    const [opened, setOpened] = useState(false);
+
+    return (
+        <>
+        <Drawer
+            opened={opened}
+            onClose={() => setOpened(false)}
+            title="Register"
+            padding="xl"
+            size="xl"
+        >
+            {/* Drawer content */}
+        </Drawer>
+
+        <Group position="center">
+            <Button onClick={() => setOpened(true)}>Open Drawer</Button>
+        </Group>
+        </>
+    );
+}

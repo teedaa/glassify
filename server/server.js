31a67fd77549703abc8c1883e64d4ef74885bce6
teedaa <pengtedachengllhs@gmail.com>
2022-12-10 17:06:39 -0800

    Add login_user mutation

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 

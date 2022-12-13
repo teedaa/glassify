9fdc405923ecb0b2d3fb5ee74e1bbd4073473526
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-12-13 12:22:33 -0500

    changed card template and added a dummy card to sidebar

--- client/src/components/SavedDrinkCard.js
+++ client/src/components/SavedDrinkCard.js
@@ -1,30 +1,30 @@
 import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
 
 export function SavedDrinkCard() {
+    // const drinkData = ({
+    //     imageLink,
+    //     drinkName,
+    // })
     return (
         <Card shadow="sm" p="lg" radius="md" withBorder>
         <Card.Section component="a" href="https://mantine.dev/">
             <Image
-            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+            src="https://www.thecocktaildb.com/images/media/drink/usspxq1441553762.jpg"           
+           // src="https://www.thecocktaildb.com/images/media/drink/4vo5651493068493.jpg"
             height={160}
-            alt="Norway"
+            alt=""
             />
         </Card.Section>
 
         <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>Norway Fjord Adventures</Text>
+            <Text weight={500}>The Addisson Special</Text>
             <Badge color="pink" variant="light">
-            On Sale
+            Alcoholic
             </Badge>
         </Group>
 
-        <Text size="sm" color="dimmed">
-            With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-            activities on and around the fjords of Norway
-        </Text>
-
         <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-            Book classic tour now
+            Remove
         </Button>
         </Card>
     );
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Drawer, Button, Group, ScrollArea } from '@mantine/core';
+import { Drawer, Button, Group, ScrollArea, Stack } from '@mantine/core';
 import { BurgerComponent } from './Burger';
 import { useDisclosure } from '@mantine/hooks';
+import { SavedDrinkCard } from './SavedDrinkCard';
 
 const drinkList = "This is the saved drinks list";
 const ingredientsList = "This is the ingredients list";
@@ -11,7 +12,6 @@ export function Sidebar() {
     const [title, setTitle] = useState("Saved Drinks");
     const [content, setContent] = useState(drinkList)
     function toggleTitle () {
-        console.log(title)
         if (title === "Saved Drinks") {
             setTitle("Ingredients List")
         } else {
@@ -40,26 +40,25 @@ export function Sidebar() {
             padding="xl"
             size="25%"
         >
-            <stack>
-                <container>
-                    <ScrollArea.Autosize maxHeight={500} sx={{maxWidth: 400}} mx="auto">
-                        <p>{content}</p>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>                      
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <Stack justify="flex-start">
+                <ScrollArea.Autosize maxHeight={500} sx={{maxWidth: 400}} mx="auto">
+                    <p>{content}</p>
+                    <SavedDrinkCard />
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>                      
+                    {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+                    {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
 
-                    </ScrollArea.Autosize>
-                </container>
-                <Group position="center">
+                </ScrollArea.Autosize>
+                <Group align="flex-end" position="center" >
                     <Button onClick={() => {
                         toggleTitle() 
                         toggleContent()}
                     }>Toggle Sidebar Content</Button>
                 </Group>
-            </stack>
+            </Stack>
         </Drawer>
 
-        <Group position="center" mb="0">
+        <Group position="left">
             <Button onClick={() => setOpened(true)}>Open Drawer</Button>
         </Group>
         

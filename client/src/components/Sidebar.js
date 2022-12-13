1db8a9706a3eff2b323560154813fa9559f48c86
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-12-13 12:09:23 -0500

    SavedDrinkCard template

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,9 +3,13 @@ import { Drawer, Button, Group, ScrollArea } from '@mantine/core';
 import { BurgerComponent } from './Burger';
 import { useDisclosure } from '@mantine/hooks';
 
+const drinkList = "This is the saved drinks list";
+const ingredientsList = "This is the ingredients list";
+
 export function Sidebar() {
     const [opened, setOpened] = useState(false);
     const [title, setTitle] = useState("Saved Drinks");
+    const [content, setContent] = useState(drinkList)
     function toggleTitle () {
         console.log(title)
         if (title === "Saved Drinks") {
@@ -14,6 +18,15 @@ export function Sidebar() {
             setTitle("Saved Drinks")
         }
         
+    }
+
+    function toggleContent () {
+        if (content === drinkList) {
+            setContent(ingredientsList)
+        } else {
+            setContent(drinkList)
+        }
+
     }
 
     return (
@@ -29,12 +42,19 @@ export function Sidebar() {
         >
             <stack>
                 <container>
-                    <ScrollArea.Autosize maxHeight={250} sx={{maxWidth: 400}} mx="auto">
+                    <ScrollArea.Autosize maxHeight={500} sx={{maxWidth: 400}} mx="auto">
+                        <p>{content}</p>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>                      
                         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
                     </ScrollArea.Autosize>
                 </container>
                 <Group position="center">
-                    <Button onClick={() => toggleTitle() }>Toggle Sidebar Content</Button>
+                    <Button onClick={() => {
+                        toggleTitle() 
+                        toggleContent()}
+                    }>Toggle Sidebar Content</Button>
                 </Group>
             </stack>
         </Drawer>

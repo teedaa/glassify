1f632e73304559f2fc0f033472952e10bf762998
Peter Kim <peter.kim@uconn.edu>
2022-12-13 23:11:47 -0500

    Burger component now opens/closes sidebar, changed drink card image height

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,71 +1,90 @@
-import React, { useState } from 'react';
-import { Drawer, Button, Group, ScrollArea, Stack } from '@mantine/core';
-// import { BurgerComponent } from './Burger';
+import React, { useState } from "react";
+import { Drawer, Button, Group, ScrollArea, Stack } from "@mantine/core";
+import { BurgerComponent } from "./Burger";
 // import { useDisclosure } from '@mantine/hooks';
-import { SavedDrinkCard } from './SavedDrinkCard';
+import { SavedDrinkCard } from "./SavedDrinkCard";
 
 const drinkList = "This is the saved drinks list";
 const ingredientsList = "This is the ingredients list";
 
 export function Sidebar() {
-    const [opened, setOpened] = useState(false);
-    const [title, setTitle] = useState("Saved Drinks");
-    const [content, setContent] = useState(drinkList)
-    function toggleTitle () {
-        if (title === "Saved Drinks") {
-            setTitle("Ingredients List")
-        } else {
-            setTitle("Saved Drinks")
-        }
-        
-    }
+	const [opened, setOpened] = useState(false);
+	const [title, setTitle] = useState("Saved Drinks");
+	const [content, setContent] = useState(drinkList);
+	function toggleTitle() {
+		if (title === "Saved Drinks") {
+			setTitle("Ingredients List");
+		} else {
+			setTitle("Saved Drinks");
+		}
+	}
 
-    function toggleContent () {
-        if (content === drinkList) {
-            setContent(ingredientsList)
-        } else {
-            setContent(drinkList)
-        }
+	function toggleContent() {
+		if (content === drinkList) {
+			setContent(ingredientsList);
+		} else {
+			setContent(drinkList);
+		}
+	}
 
-    }
+	return (
+		<>
+			<Drawer
+				opened={opened}
+				onClose={() => setOpened(false)}
+				// title will be where we switch the List Title
+				title={title}
+				position="left"
+				padding="xl"
+				size="25%"
+			>
+				<Stack justify="flex-start">
+					<ScrollArea.Autosize
+						// maxHeight={500}
+						sx={{ maxWidth: 500 }}
+						mx="auto"
+						offsetScrollbars
+					>
+						<p>{content}</p>
+						<SavedDrinkCard />
+						<p>
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum."
+						</p>
+						{/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+						{/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+					</ScrollArea.Autosize>
+					<Group align="flex-end" position="center">
+						<Button
+							onClick={() => {
+								toggleTitle();
+								toggleContent();
+							}}
+						>
+							Toggle Sidebar Content
+						</Button>
+					</Group>
+				</Stack>
+			</Drawer>
 
-    return (
-        <>
-        <Drawer
-            opened={opened}
-            onClose={() => setOpened(false)}
-            // title will be where we switch the List Title
-            title={title}
-            position="left"
-            padding="xl"
-            size="25%"
-        >
-            <Stack justify="flex-start">
-                <ScrollArea.Autosize maxHeight={500} sx={{maxWidth: 500}} mx="auto" offsetScrollbars>
-                    <p>{content}</p>
-                    <SavedDrinkCard />
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>                      
-                    {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
-                    {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
+			{/* <Group position="left">
+				<Button onClick={() => setOpened(true)}>Open Drawer</Button>
+			</Group> */}
 
-                </ScrollArea.Autosize>
-                <Group align="flex-end" position="center" >
-                    <Button onClick={() => {
-                        toggleTitle() 
-                        toggleContent()}
-                    }>Toggle Sidebar Content</Button>
-                </Group>
-            </Stack>
-        </Drawer>
-
-        <Group position="left">
-            <Button onClick={() => setOpened(true)}>Open Drawer</Button>
-        </Group>
-        
-        {/* <Group position="center">
-            <BurgerComponent onClick={() => setOpened(true)}>Open Drawer</BurgerComponent>
-        </Group> */}
-        </>
-    );
+			<Group position="center">
+				<BurgerComponent
+					setBurgerOpen={setOpened}
+					burgerOpened={opened}
+					onClick={() => setOpened(true)}
+				>
+					Open Drawer
+				</BurgerComponent>
+			</Group>
+		</>
+	);
 }
-
